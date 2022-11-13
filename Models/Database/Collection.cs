3b77549fb970772e97944de5fbe8f0b73fb63da5
Merthynz <rerive@gmail.com>
2022-11-12 22:57:18 -0500

    Revert "Revert "Added Collection model""
    This reverts commit fdf20af6f2bcaa40f3acddec2651bc6f330ac112.

---
+++ Models/Database/Collection.cs
@@ -0,0 +1,11 @@
+﻿#nullable disable
+
+namespace MovieProDemo.Models.Database
+{
+    public class Collection
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
