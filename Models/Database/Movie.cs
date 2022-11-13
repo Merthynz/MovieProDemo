0f163bde7e1e88bbc9814a1381051f7310029695
Merthynz <rerive@gmail.com>
2022-11-12 22:57:39 -0500

    Revert "Revert "Added MovieCrew model, navigational property to Collection and Movie model""
    This reverts commit 4e5c1302192d9aa786f04393181c11eb1b26351f.

--- Models/Database/Movie.cs
+++ Models/Database/Movie.cs
@@ -39,6 +39,8 @@ namespace MovieProDemo.Models.Database
         [Display(Name = "Backdrop Image")]
         public IFormFile BackdropFile { get; set; }
 
-
+        public ICollection<MovieCollection> Collections { get; set; } = new HashSet<MovieCollection>();
+        public ICollection<MovieCast> Cast { get; set; } = new HashSet<MovieCast>();
+        public ICollection<MovieCrew> Crew { get; set; } = new HashSet<MovieCrew>();
     }
 }
