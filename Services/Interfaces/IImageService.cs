66bea186736bcf7c520301c7bb5692040b2705e6
Merthynz <rerive@gmail.com>
2022-11-12 22:46:36 -0500

    Revert "Added interfaces"
    This reverts commit ab075c89c119f01655ef482b9c984a483b85f4fb.

--- Services/Interfaces/IImageService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MovieProDemo.Services.Interfaces
-{
-    public interface IImageService
-    {
-        Task<byte[]> EncodeImageAsync(IFormFile poster);
-        Task<byte[]> EncodeImageURLAsync(string imageURL);
-        string DecodeImage(byte[] poster, string contentType);
-    }
-}
