dbf403611501d930137a3966a6089f59be4977ab
Merthynz <rerive@gmail.com>
2022-11-12 22:57:47 -0500

    Revert "Revert "Added a ViewModel""
    This reverts commit 0419ac80688e68fa792f52caf6a3d176bbe065c1.

---
+++ Models/ViewModels/LandingPageVM.cs
@@ -0,0 +1,16 @@
+﻿using MovieProDemo.Models.Database;
+using MovieProDemo.Models.TMDB;
+
+#nullable disable
+
+namespace MovieProDemo.Models.ViewModels
+{
+    public class LandingPageVM
+    {
+        public List<Collection> CustomCollections { get; set; }
+        public MovieSearch NowPlaying { get; set; }
+        public MovieSearch Popular { get; set; }
+        public MovieSearch TopRated { get; set; }
+        public MovieSearch Upcoming { get; set; }
+    }
+}
